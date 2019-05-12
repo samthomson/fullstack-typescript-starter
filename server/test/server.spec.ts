f9a0f33479cc36b2127b0353ec388ed6cfa50f30
Sam Thomson <samt@samt.st>
2019-05-12 16:07:58 +0000

    change tests to use server

--- server/test/server.spec.ts
+++ server/test/server.spec.ts
@@ -1,7 +1,7 @@
 import 'mocha'
-import axios from 'axios'
 import * as chai from 'chai'
 import chaiHttp = require('chai-http')
+import server from './../src/server'
 
 chai.use(chaiHttp)
 
@@ -9,20 +9,20 @@ describe('server', async () => {
 	describe('get server time', async () => {
 		it('should fetch serverTime as expected', async () => {
 			// post message with correct paramameters and reasonable values
-			const oServerTimeResponse = await axios.post(
-				'http://server:3100/graphql',
-				{
+			const oServerTimeResponse = await chai
+				.request(server)
+				.post('/graphql')
+				.send({
 					query: `
 			  {
 				serverTime
 			  }`,
-				},
-			)
+				})
 
-			chai.expect(oServerTimeResponse.data.data.serverTime).to.be.an(
+			chai.expect(oServerTimeResponse.body.data.serverTime).to.be.an(
 				'number',
 			)
-			chai.expect(oServerTimeResponse.data.data.serverTime).to.be.above(0)
+			chai.expect(oServerTimeResponse.body.data.serverTime).to.be.above(0)
 		})
 	})
 })
